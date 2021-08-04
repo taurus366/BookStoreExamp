49af1523063ee896fc28f0f94b5589dbf2f6f9a7
taurus366 <47596673+taurus366@users.noreply.github.com>
2021-08-04 23:58:24 +0300

    added header,home,footer,contact us component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./core/home/home.component";
+import {BooksComponent} from "./core/books/books.component";
+import {RegisterComponent} from "./core/register/register.component";
+import {LoginComponent} from "./core/login/login.component";
+import {ContactComponent} from "./core/contact/contact.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'books',
+    component: BooksComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,19 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
+import {RouterModule} from "@angular/router";
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 
 @NgModule({
     declarations: [
         HeaderComponent,
-        FooterComponent
+        FooterComponent,
+        HomeComponent,
+        BooksComponent,
+        RegisterComponent,
+        LoginComponent,
+        ContactComponent
     ],
-    exports: [
-        HeaderComponent
-    ],
-    imports: [
-        CommonModule
-    ]
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
 })
 export class CoreModule { }
